4d3f401965cf0bc465eb825d418d3fdf991b0223
Ankur Parihar <pariharankur101@gmail.com>
2022-02-13 18:22:49 +0530

    Added ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {},
+};
